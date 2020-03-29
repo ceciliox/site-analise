fbd0f600e6e012f28d35a767d32d96163a2e0b9a
ceciliox <49498975+ceciliox@users.noreply.github.com>
2020-03-29 19:09:23 -0300

    Update envia_dados.sh

--- kafka/envia_dados.sh
+++ kafka/envia_dados.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+: '
 export PATH=$PATH:/usr/hdp/current/kafka-broker/bin
 ARQUIVOS=$1/*.csv
 for f in $ARQUIVOS
+do
+    echo "buscando $f arquivos"
+    cat $f | kafka-console-producer.sh --broker-list $2  --topic $3
+    sleep 60
+done
+'
+export PATH=$PATH:/usr/hdp/current/kafka-broker/bin
+ARQUIVOS=$1/out.csv
+while :
 do
     echo "buscando $f arquivos"
     cat $f | kafka-console-producer.sh --broker-list $2  --topic $3
